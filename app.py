55d61e871d722a07f3a6c2c6ebe18190c5d1090e
Sandeep <sandeepg.97@gmail.com>
2018-11-29 13:25:11 +0530

    added login password and feedback messages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect, url_for
 import os, sys 
 
 app = Flask(__name__)
@@ -8,21 +8,31 @@ UPLOAD_FOLDER = '/home/s4nd33p/Downloads/share-it'
 @app.route("/",methods = ['GET','POST'])
 def homepage():
 	if request.method == 'GET':	
-		return render_template("homepage.html")
+		return render_template("login.html")
 	else:
-		# file = request.files['file1']
-		# print(file)
-		for i in xrange(1,6):
-			if 'file' + str(i) in request.files:
-				file = request.files['file' + str(i)]
-				if file.filename != '':
-					file.save(os.path.join(app.config['UPLOAD_FOLDER'],file.filename))
-		# flash('Files sent!')
-		return render_template("homepage.html")
+		if ("password" in request.form) and (request.form["password"] == "shareit"):
+			return render_template("homepage.html")
+		else:
+			flash("Invalid password!")
+			return render_template("login.html")
+
+@app.route("/upload",methods = ['POST'])
+def upload():
+	count = 0
+	for i in xrange(1,6):
+		if 'file' + str(i) in request.files:
+			file = request.files['file' + str(i)]
+			if file.filename != '':
+				file.save(os.path.join(app.config['UPLOAD_FOLDER'],file.filename))
+				count += 1
+	flash(str(count) + " files sent! Sign-in to send again...")
+	return redirect(url_for('homepage'))
+
 
 def main():
 	port = int(os.environ.get("PORT", 8000))
 	app.debug = True
+	app.secret_key = 'thequickbrownfoxjumpsoverthelazydog'
 	app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 	app.run(host = "0.0.0.0", port = port)
 
